7fe8b61c87c7baf48f6728990f55835186c1d681
Tanba28 <wa2.n893@gmail.com>
2020-05-21 20:52:19 +0900

    add python source for displaying UI

---
+++ source/mainwindow.py
@@ -0,0 +1,8 @@
+from PyQt5 import QtWidgets, QtCore, QtSerialPort
+from ui_serialmonitor import Ui_MainWindow
+
+class MainWindow(QtWidgets.QMainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)       
